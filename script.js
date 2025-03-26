ae879ea30b5d85ffcc5e13a23e65f387497c66a4
Prathampthk <prathampthk13@gmail.com>
2025-03-26 10:39:39 -0500

    Added Projects section

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Memer", "Designer", "Freelancer"],
+        strings: ["Student", "Freelancer", "Designer", "Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Memer", "Designer", "Freelancer"],
+        strings: ["Student", "Freelancer", "Designer", "Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
